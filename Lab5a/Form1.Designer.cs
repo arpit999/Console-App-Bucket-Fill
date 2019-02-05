5ecbcb8d490397d3ed9801c7984b9feabaebf412
Flutter <arpitpatel009@gmail.com>
2019-02-05 14:20:33 -0500

    Add project files.

---
+++ Lab5a/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Lab5a
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonColor = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonColor
+            // 
+            this.buttonColor.Location = new System.Drawing.Point(62, 26);
+            this.buttonColor.Name = "buttonColor";
+            this.buttonColor.Size = new System.Drawing.Size(75, 23);
+            this.buttonColor.TabIndex = 0;
+            this.buttonColor.Text = "Color";
+            this.buttonColor.UseVisualStyleBackColor = true;
+            this.buttonColor.Click += new System.EventHandler(this.buttonColor_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(229, 26);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 0;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox.Image")));
+            this.pictureBox.Location = new System.Drawing.Point(117, 132);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(92, 65);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // trackBar
+            // 
+            this.trackBar.Location = new System.Drawing.Point(117, 73);
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(104, 45);
+            this.trackBar.TabIndex = 5;
+            this.trackBar.Scroll += new System.EventHandler(this.trackBar_Scroll);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(443, 465);
+            this.Controls.Add(this.trackBar);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonColor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonColor;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Lab5a/Form1.cs
@@ -0,0 +1,136 @@
+﻿/*
+  Class:  Form1.cs
+  Author: Arpit Patel
+  Student ID: 000762465
+  Date:   Decembner 7, 2018
+
+*/
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab5a
+{
+    public partial class Form1 : Form
+    {
+        int y = 400;
+        private Graphics g;                         //Encapsulates a GDI+ drawing surface
+        private Pen lineColor,bucketFillColor;                              //Pens are used to draw objects
+        private Font f;                             //Defines a particular format for text, including font face, size, and style attributes
+        private SolidBrush b;                       //Brushes are used to fill graphics shapes
+        private Color c = Color.Blue;              //Represents a color, initially set to black
+        SolidBrush bucketColor ;
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += new PaintEventHandler(frmMain_Paint);  //Registers the Paint event handler
+
+            bucketColor = new SolidBrush(Color.White);
+        }
+
+        /// <summary>
+        /// set main fram for the form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmMain_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;                          //Get the Graphics object from the PaintEventArgs
+            lineColor = new Pen(Color.Black);                          //Create a new Pen using the current colour
+           
+            b = new SolidBrush(c);                   //Create a new brush using the current colour
+
+            g = this.CreateGraphics();               //Create a graphics object
+
+            g.DrawLine(lineColor, 160, 300, 160, 400);        //Draw two intersecting lines using the current Pen
+            g.DrawLine(lineColor, 160, 400, 350, 400);
+            g.DrawLine(lineColor, 350, 400, 350, 300);
+
+           
+
+        }
+
+
+        /// <summary>
+        /// Choose color using this button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonColor_Click(object sender, EventArgs e)
+        {
+            colorDialog.Color = c;                //Display with the previous colour already chosen
+            colorDialog.ShowDialog();             //Display the actual dialog box
+            c = colorDialog.Color;                //Save the colour choice the user made
+        }
+
+        /// <summary>
+        /// Close form usin this button click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+
+            System.Windows.Forms.Application.Exit();
+        }
+
+        /// <summary>
+        /// tick bar event for increase the speed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void trackBar_Scroll(object sender, EventArgs e)
+        {
+            
+            timer.Interval = 120 - trackBar.Value * 10;
+            
+            b = new SolidBrush(c);
+            bucketFillColor = new Pen(c);
+
+           // trackBar.Minimum = 1;
+            //trackBar.Maximum= 10;
+            //trackBar.TickFrequency = 10;
+            //trackBar.LargeChange = 100;
+            //trackBar.SmallChange = 10;
+
+            if (trackBar.Value > 0 )
+            {
+                g = this.CreateGraphics();
+                g.FillRectangle(b, 190, 185, 14, 215);
+                timer.Start();
+            }
+            
+
+            timer.Interval = 120 - trackBar.Value * 11;
+            
+        }
+
+        /// <summary>
+        /// Timer tick listener stop and set background color in inside bucket and rectangle 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer_Tick(object sender, EventArgs e)
+        {
+
+            y--;
+            g.DrawLine(bucketFillColor, 349, y, 161, y);
+
+            if (y == 300)
+            {
+                y = 400;
+                timer.Enabled = false;
+                timer.Stop();
+                 
+                g.FillRectangle(bucketColor, 161,300,189,100);
+                g.FillRectangle(bucketColor, 190, 182, 14, 214);
+            }
+
+        }
+
+
+    }
+}
